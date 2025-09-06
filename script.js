2abbdbd6a1899cc82240b0b9b4a5deeae428036f
Anonnya01 <anonnya1999@gmail.com>
2025-09-07 00:21:47 +0600

    nav & header done

---
+++ script.js
@@ -0,0 +1,26 @@
+const getCategoryBtn=document.getElementById("category-btn");
+
+const getCategory = () => {
+  fetch("https://openapi.programming-hero.com/api/categories")
+    .then((res) => res.json())
+    .then((data) => {
+      const categories=data.categories
+      showCategoryBtn(categories);
+    })
+
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const showCategoryBtn=(categories)=>{
+console.log(categories);
+categories.forEach(category => {
+  getCategoryBtn.innerHTML +=`
+   
+   <button class=" hover:bg-[#15803D] hover:text-white text-left w-full p-3 rounded-lg">${category.category_name}</button>
+  `
+});
+
+}
+getCategory()

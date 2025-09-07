9762e1e80271330f7e1bdb73d416a4cdd6e99055
Anonnya01 <anonnya1999@gmail.com>
2025-09-07 20:26:10 +0600

    body part done

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const getCategoryBtn=document.getElementById("category-btn");
 
+// -----card container----------
+const cardContainer = document.getElementById("card-cont")
+
+// ---------get category btn-----------//
 const getCategory = () => {
   fetch("https://openapi.programming-hero.com/api/categories")
     .then((res) => res.json())
@@ -12,15 +16,31 @@ const getCategory = () => {
       console.log(err);
     });
 };
-
+//----------- show category btn-------------//
 const showCategoryBtn=(categories)=>{
 console.log(categories);
 categories.forEach(category => {
   getCategoryBtn.innerHTML +=`
    
-   <button class=" hover:bg-[#15803D] hover:text-white text-left md:w-full p-3 rounded-lg border-[#15803D] border-1 md:border-none ">${category.category_name}</button>
+   <button class=" hover:bg-[#15803D] hover:text-white text-left md:w-full p-3 rounded-lg
+    border-[#15803D] border-1 md:border-none text-[14px] md:text-base font-semibold">${category.category_name}</button>
   `
+
 });
 
 }
+// get plants card//
+const getCardCategory =(id)=>{
+  fetch("https://openapi.programming-hero.com/api/category/${id}")
+  .then(res => res.json())
+  .then((data)=>{
+    console.log(data.category_name);
+    
+  })
+  .catch((err) => {
+      console.log(err);
+    });
+}
+
+getCardCategory()
 getCategory()

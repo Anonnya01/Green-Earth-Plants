6c16ed9e3c42c44742e2354b754117952b49cf8e
Anonnya01 <anonnya1999@gmail.com>
2025-09-07 13:32:58 +0600

    responsive style added

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ console.log(categories);
 categories.forEach(category => {
   getCategoryBtn.innerHTML +=`
    
-   <button class=" hover:bg-[#15803D] hover:text-white text-left w-full p-3 rounded-lg">${category.category_name}</button>
+   <button class=" hover:bg-[#15803D] hover:text-white text-left md:w-full p-3 rounded-lg border-[#15803D] border-1 md:border-none ">${category.category_name}</button>
   `
 });
 
